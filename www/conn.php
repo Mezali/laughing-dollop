79adf9eaef60f74d3c38754123ef16717516705b
Mezali <danieldelimamazali@hotmail.com>
2022-08-12 10:01:00 -0300

    f

--- www/conn.php
+++ www/conn.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 try {
-    $pdo = new PDO("mysql:host=database;dbname=docker", "root", "tiger");
+    $pdo = new PDO("mysql:host=banco;dbname=docker", "docker", "docker");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
