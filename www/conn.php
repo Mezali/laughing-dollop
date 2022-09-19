7866382b858060173b9ec34a567b4c476b5b7048
Mezali <danieldelimamazali@hotmail.com>
2022-09-19 17:21:39 -0300

    passivo agressivo

--- www/conn.php
+++ www/conn.php
@@ -3,6 +3,6 @@ try {
     $pdo = new PDO("mysql:host=banco;dbname=docker", "docker", "docker");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("ERROR: Could not connect. " . $e->getMessage());
+    die("PHP é uma bosta: o banco deu pau, vai lá resolver isso seu merda" . $e->getMessage());
 }
 // bosta de php, odeio php, não gosto de php
