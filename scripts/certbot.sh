df148a79a64a02616c7fd8718a0427dcb8271f36
Ubuntu <mezali@staging.ppnxncs5jvfuxlkeptfjd15g0e.nx.internal.cloudapp.net>
2022-10-30 12:59:49 +0000

    AEEEE  CARALHOOO FOIII

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!bin/bash
 
-#certbot run -n --apache --agree-tos -d certbot.brazilsouth.cloudapp.azure.com -m danieldelimamazali@hotmail.com --redirect
-echo "MERDAAAA"
+certbot run -n --apache --agree-tos -d certbot.brazilsouth.cloudapp.azure.com -m  danieldelimamazali@hotmail.com  --redirect
+
+while true; do sleep 1000; done
