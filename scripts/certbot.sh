32a2ab27637f0e89344de3a6550af0dacd3c2dbb
Mezali <danieldelimamazali@hotmail.com>
2022-11-06 16:46:57 +0000

    gitignore

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,5 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
-certbot run -n --apache --agree-tos -d sgpo.online  -m  danieldelimamazali@hotmail.com  --redirect
+certbot run -n --apache --agree-tos -d www.sgpo.online -m danieldelimamazali@hotmail.com --redirect
 
 while true; do sleep 1000; done
+
