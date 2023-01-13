f83c22d9f1e1d028abf85fb06b4ce5635174396e
Mezali <danieldelimamazali@hotmail.com>
2023-01-13 10:45:42 -0300

    rm

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /home/mezali/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/mezali/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
